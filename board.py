c82d4befd3dfbe36ebb6063fa57139d116d97ab5
fatiyaa <fatiyaizzati26@gmail.com>
2023-11-19 22:20:27 +0700

    fix color

--- board.py
+++ board.py
@@ -120,6 +120,7 @@ class Board:
             self.possible_moves = []
             return
         
+        self.possible_moves = []
         for row in range(RC):
             for col in range(RC):
                 if self.board[row][col] != [] and self.board[row][col][-1].color == self.turn:
@@ -180,7 +181,7 @@ class Board:
         pygame.draw.rect(screen, BG, (20, SCREEN_HEIGHT//3, SCREEN_WIDTH-40, SCREEN_HEIGHT//3))
         pygame.draw.rect(screen, BLACK, (20, SCREEN_HEIGHT//3, SCREEN_WIDTH-40, SCREEN_HEIGHT//3), 4)
         font = pygame.font.SysFont(None, 100)
-        text = font.render("IT'S A TIE", True, GREEN);
+        text = font.render("IT'S A TIE", True, ORANGE);
         if win == RED:
             text = font.render('RED IS WIN', True, RED);
         if win == BLUE:
@@ -188,7 +189,7 @@ class Board:
         screen.blit(text, text.get_rect(center = (SCREEN_WIDTH//2, SCREEN_HEIGHT//2 -20)))
         
         font = pygame.font.SysFont(None, 50)
-        play_again_button = Button(200, SCREEN_HEIGHT//2+40, 'Play Again', font, BLACK, GREEN)
+        play_again_button = Button(200, SCREEN_HEIGHT//2+40, 'Play Again', font, BLACK, BLUE)
         quit_button = Button(400, SCREEN_HEIGHT//2+40, 'Quit Game', font, BLACK, RED )
         
         for button in [play_again_button, quit_button]:
